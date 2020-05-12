763d56b4063741671da9eec6d52bcbc487f87837
emre8en <emre8en@koji-projects.com>
2020-05-12 19:16:32 +0000

    Manual deploy

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { InstantRemixing } from '@withkoji/vcc';
+
+const instantRemixing = new InstantRemixing();
+
+let name = instantRemixing.get(['settings', 'name']);
+let title = instantRemixing.get(['settings', 'title']);
+let logo = instantRemixing.get(['logo', 'logo']);
+
+instantRemixing.onValueChanged((path, newValue) => {
+	if (path[0] && path[1] && path[0] === 'settings' && path[1] === 'name') {
+		name = newValue;
+	}
+});
+
+instantRemixing.ready();
 
 const Container = styled.div`
-    background-color: #395b88;
+    background-color: red;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
@@ -19,14 +34,15 @@ const Image = styled.img`
 `;
 
 class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <h1>{'Welcome to Your App'}</h1>
-        <Image src={'https://images.koji-cdn.com/d9c6b38e-08c4-4faf-ae8e-01082c41a0fb/3f83q-9634d620e97345a6b250ca2feb7e5a2e.png'} />
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<h1>{name}</h1>
+				<h2>{title}</h2>
+				<Image src={logo} />
+			</Container>
+		);
+	}
 }
 
 export default App;
